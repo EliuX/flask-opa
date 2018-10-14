8a17e69fc4d48ec092c07de4b36c713a8da1d13d
EliuX <eliecerhdz@gmail.com>
2018-10-13 19:44:36 -0500

    First commit: Setup project structure

---
+++ README.rst
@@ -0,0 +1,4 @@
+Flask-OPA
+=========
+Simple to use `Flask <http://flask.pocoo.org/>`_ extension that lets you use
+`Open Policy Agent <https://www.openpolicyagent.org>`_ in your project.
---
+++ setup.py
@@ -0,0 +1,35 @@
+"""
+Flask-OPA
+-------------
+
+Flask extension that lets you use Open Policy Agent (OPA) in your project
+as a client
+"""
+from setuptools import setup
+
+setup(
+    name='Flask-OPA',
+    version='0.1',
+    url='https://github.com/EliuX/Flask-OPA',
+    license='MIT',
+    author='Eliecer Hernandez',
+    author_email='eliecerhdz@gmail.com',
+    description='Flask extension to use OPA as a client',
+    long_description=__doc__,
+    py_modules=['flask-opa'],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'Flask'
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
